f79fdc55d030f428dc5c6b3d54e6e84fba305021
Konstantin Komarov <almaz.alexandrovich@paragon-software.com>
2024-06-17 14:53:57 +0300

    fs/ntfs3: Check more cases when directory is corrupted
    [ Upstream commit 744375343662058cbfda96d871786e5a5cbe1947 ]
    
    Mark ntfs dirty in this case.
    Rename ntfs_filldir to ntfs_dir_emit.
    
    Signed-off-by: Konstantin Komarov <almaz.alexandrovich@paragon-software.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- fs/ntfs3/dir.c
+++ fs/ntfs3/dir.c
@@ -318,6 +318,23 @@ static inline bool ntfs_dir_emit(struct ntfs_sb_info *sbi,
 	else
 		dt_type = (fname->dup.fa & FILE_ATTRIBUTE_DIRECTORY) ? DT_DIR : DT_REG;
 
+	/*
+	 * It is not reliable to detect the type of name using duplicated information
+	 * stored in parent directory.
+	 * The only correct way to get the type of name - read MFT record and find ATTR_STD.
+	 * The code below is not good idea.
+	 * It does additional locks/reads just to get the type of name.
+	 * Should we use additional mount option to enable branch below?
+	 */
+	if ((fname->dup.fa & FILE_ATTRIBUTE_REPARSE_POINT) &&
+	    ino != ni->mi.rno) {
+		struct inode *inode = ntfs_iget5(sbi->sb, &e->ref, NULL);
+		if (!IS_ERR_OR_NULL(inode)) {
+			dt_type = fs_umode_to_dtype(inode->i_mode);
+			iput(inode);
+		}
+	}
+
 	return dir_emit(ctx, (s8 *)name, name_len, ino, dt_type);
 }
 
@@ -461,7 +478,6 @@ static int ntfs_readdir(struct file *file, struct dir_context *ctx)
 
 		vbo = (u64)bit << index_bits;
 		if (vbo >= i_size) {
-			ntfs_inode_err(dir, "Looks like your dir is corrupt");
 			err = -EINVAL;
 			goto out;
 		}
